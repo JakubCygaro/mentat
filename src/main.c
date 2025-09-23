ddafa41a8bee9eb1f59ccb49c82b028b4cba4ea4
papieros <robertrugala@gmail.com>
2025-09-23 14:53:21 +0200

    aha ok

--- src/main.c
+++ src/main.c
@@ -3,14 +3,6 @@
 #include "mentat_app.h"
 #include "state.h"
 
-// this sucks ass
-char get_decimal_separator(){
-    char test_str[9] = { 0 };
-    sprintf(test_str, "%f", 0.0);
-    printf("%s\n", test_str);
-    return 0;
-}
-
 
 int main(void){
     MentatApp* app;
--- src/mentat_app.c
+++ src/mentat_app.c
@@ -1,23 +1,36 @@
 #include "mentat_app.h"
+#include <glib.h>
 #include <gtk/gtk.h>
 #include "mentatui.h"
 #include "state.h"
 
 
+// static char css[] =
+// "#text_box{ font: 48px; color: magenta; }";
+//
+
+
 static GtkWindow* mentat_window_new(MentatApp* app) {
     GtkBuilder* builder = gtk_builder_new_from_string(UI_STRING, strlen(UI_STRING));
+    GtkCssProvider* provider = gtk_css_provider_new();
 
     GObject* window = gtk_builder_get_object(builder, "mentat_window");
     gtk_window_set_application(GTK_WINDOW(window), GTK_APPLICATION(app));
-
     GObject* text_view = gtk_builder_get_object(builder, "text_box");
     GtkTextBuffer* text_buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
+
+    GdkDisplay* display = gdk_display_get_default();
+    gtk_style_context_add_provider_for_display(display, GTK_STYLE_PROVIDER(provider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+    // GBytes* css_bytes = g_bytes_new(css, strlen(css));
+    // gtk_css_provider_load_from_bytes(provider, css_bytes);
+    //
 
     app->state->text_buffer = text_buffer;
 
     gtk_widget_set_visible(GTK_WIDGET(window), TRUE);
-
     g_object_unref(builder);
+    g_object_unref(provider);
     return GTK_WINDOW(window);
 }
 
